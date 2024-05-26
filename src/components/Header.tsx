33687dad568dbf6f69a1be56cc7d435278e23c1f
bobetbat <b.bobetayev.97@gmail.com>
2024-05-26 10:57:23 +0200

    feat: imgs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import { Stack } from '@mui/material';
+import logo from '../logo.png'; // Adjust the path to match your file structure
 
 export const Header: React.FC = () => {
   return (
@@ -11,10 +12,10 @@ export const Header: React.FC = () => {
       <Box maxWidth="xl">
         <Toolbar
           disableGutters
-          sx={{ justifyContent: 'space-between', px: '1rem', py: '0.5rem',backgroundColor:'hsla(0,0%,100%,.65)' }}
+          sx={{ justifyContent: 'space-between', px: '1rem', py: '0.5rem', backgroundColor: 'hsla(0,0%,100%,.65)' }}
         >
           <Stack direction='row' gap={2}>
-            {/* <ConnectButton /> */}
+            <img src={logo} alt="Logo" style={{ height: '64px' }} />
           </Stack>
         </Toolbar>
       </Box>
